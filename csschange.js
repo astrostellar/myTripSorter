b83c70d44d0ba3f49658a16e4e6df52767cfc792
astrostellar <749625@gmail.com>
2015-08-20 17:00:10 +0300

    add right version of file
    csschange.js

--- csschange.js
+++ csschange.js
@@ -3,15 +3,56 @@ $ = (function() {
     var a = {};
     a._el = document.querySelector(str);
     a.add = function(str) {
-      if (!this._el.classList.contains(str)) {
-        this._el.classList.add(str);
+      var
+        update = false,
+        arr;
+      if (!a.check(str)) {
+        update = true;
+        arr = this._el.className.split(' ');
+        arr.push(str);
+        this._el.className = arr.join(' ');
       }
+
+
+      if (update === true) {
+        this._el.setAttribute("class", this._el.className.toString());
+      }
+
       return this;
     };
 
+    a.check = function(str) {
+      var
+        elClass = str,
+        arr = this._el.className.split(' '),
+        sum = 0;
+      for (var i = 0, l = arr.length; i < l; i++) {
+        if (arr[i] === elClass) {
+          sum++;
+        }
+      }
+
+      return (sum === 0 ? false : true);
+    };
+
     a.remove = function(str) {
-      if (this._el.classList.contains(str)) {
-        this._el.classList.remove(str);
+      var
+        update = false,
+        arr;
+      if (a.check(str)) {
+        update = true;
+        arr = this._el.className.split(' ');
+        for (var i = 0, l = arr.length; i < l; i++) {
+          if (arr[i] === str) {
+            arr[i] = '';
+          }
+        }
+
+        this._el.className = arr.join(' ');
+      }
+
+      if (update === true) {
+        this._el.setAttribute("class", this._el.className.toString());
       }
 
       return this;
